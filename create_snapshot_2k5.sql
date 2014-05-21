9cab946b0898ba6a3728235e6968123aa462f21d
georgebuckerfield <georgebuckerfield@gmail.com>
2014-05-21 14:30:18 +0100

    Added various scripts

---
+++ create_snapshot_2k5.sql
@@ -0,0 +1,35 @@
+/*
+SCRIPT CREATES DATABASE SNAPSHOT OF THE CURRENT DB.
+CHANGE THE <DATABASE_NAME> AT THE TOP OF THE SCRIPT.
+*/
+use eGMS
+go
+
+declare @name varchar(200)
+declare @datapath varchar(2000)
+declare @logicalname varchar(250)
+declare @sqltext varchar(3000)
+declare @snapshotname varchar(200)
+declare @snapdate varchar(8)
+declare @datafilename varchar(200)
+
+
+select @snapdate = convert(varchar, getdate(), 112)
+
+select @name = db_name()
+
+select @snapshotname = @name + '_snapshot_' + @snapdate
+
+select @datafilename = right([physical_name],charindex('\',reverse ([physical_name]))-1) from master.sys.master_files where database_id = db_id() and file_id = 1
+
+select @datapath = substring(physical_name, 1, charindex(@datafilename, lower(physical_name)) - 1) from sys.database_files where file_id = 1
+
+select @datapath = @datapath + @name + '_' + @snapdate + '_snapshot.ss'
+
+select @logicalname = name from sys.database_files where file_id = 1
+
+set @sqltext = 'create database ' + @snapshotname + ' on (Name = ' + @logicalname + ', filename = ''' + @datapath + ''') as snapshot of ' + @name + ';'
+
+print @sqltext
+
+exec(@sqltext)
